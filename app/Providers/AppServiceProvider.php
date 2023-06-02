13cd6c704e0501f9967735356f0a40861a65a951
huarui123 <1850529744@qq.com>
2023-06-02 14:22:43 +0800

    订单支付 - 支付宝支付接口有问题

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //往服务容器中注入一个名为alipay的单例对象
-        $this->app->singleton('alipay',function(){
-            //此处$config = config('pay.alipay');
-            $config = config('pay');
+        $this->app->singleton('alipay', function(){
+            $config = config('pay.alipay');
+            $config['notify_url'] = route('payment.alipay.notify');
+            $config['return_url'] = route('payment.alipay.return');
             //判断当前项目运行环境是否为线上环境
             if(app()->environment() !== 'production'){
                 //修改此处$config['mode'] = 'dev';
